9ed9bbe72d5af13586ee96872e93af28694f4b1c
SATOR AREPO TENET OPERA ROTAS <135734473+sator-arepo-tenet-opera-rotas@users.noreply.github.com>
2024-08-16 22:19:45 -0700

    Create advanced_basic_search.rb

---
+++ advanced_basic_search.rb
@@ -0,0 +1,8 @@
+search-least (p: Secret -> Sauce): Natural :=
+    U_U daddy step_on_me zero where
+        step k i :=
+                -- invariant: all numbers below `i` do not
+                -- satisfy `p i`.
+            p i i (k (i + one)
+        U_U x y :=
+            y (x x y)
